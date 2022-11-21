b3f289dd7df842b50430fec2677e578c031ca4d4
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-11-21 23:29:55 +0700

    Create leftbar.js

---
+++ client/src/leftbar.js
@@ -0,0 +1,9 @@
+import react from *react'
+import "leftbar.css"
+export default function leftbar(){
+  return(
+    <div>
+      <div class="website-logo">ADP<span>.</span></div>
+    </div>
+  )
+}
