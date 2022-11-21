6a0fbe8ab3cde4a6aed3fea8aad84aeab649b72a
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-11-21 23:55:21 +0700

    Update leftbar.js

--- client/src/component/leftbar.js
+++ client/src/component/leftbar.js
@@ -4,6 +4,23 @@ export default function leftbar(){
   return(
     <div class="LeftbarMainContainer">
       <div class="website-logo">ADP<span class="dot">.</span></div>
+      <div>
+        <p>Menu</p>
+        <ul>
+            <li>
+                <img src="" alt=""/>
+                <p class ="leftbar-item">Browse</p>
+            </li>
+            <li>
+                <img src="" alt=""/>
+                <p class ="leftbar-item">Watchlist</p>
+            </li>
+            <li>
+                <img src="" alt=""/>
+                <p class ="leftbar-item">Coming Soon</p>
+            </li>
+        </ul>
+      </div>
     </div>
   )
 }
