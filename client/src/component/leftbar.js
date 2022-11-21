2e6f605a1f70cc6c215de44f3072a30ef2a1764b
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-11-21 23:47:37 +0700

    Create component for web

---
+++ client/src/component/leftbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "leftbar.css"
+export default function leftbar(){
+  return(
+    <div class="LeftbarMainContainer">
+      <div class="website-logo">ADP<span class="dot">.</span></div>
+    </div>
+  )
+}
