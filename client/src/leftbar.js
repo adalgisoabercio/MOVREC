87042b412186947aa87129bacb65380ddb5dc680
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-11-21 23:46:50 +0700

    Delete leftbar.js

--- client/src/leftbar.js
+++
@@ -1,9 +0,0 @@
-import react from *react'
-import "leftbar.css"
-export default function leftbar(){
-  return(
-    <div class="LeftbarMainContainer">
-      <div class="website-logo">ADP<span class="dot">.</span></div>
-    </div>
-  )
-}
