18fa3f7a784e76540a19cd076c1f929acaa008d7
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-12-04 17:52:10 +0700

    Update Navbar.js

--- client/src/Component/Navbar/Navbar.js
+++ client/src/Component/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Navbar.css';
 const Navbar = () => {
-    <script src="https://kit.fontawesome.com/b4aece989d.js" crossorigin="anonymous"></script>
     var navbar = document.querySelector ('#navbar');
     document.addEventListener('scroll', () => { 
         var scroll_position = window.scrollY;
@@ -14,31 +13,31 @@ const Navbar = () => {
     return(
         
         <div id ="navbar" className="navbar">
-        <div className="container">
-            <div className="left">
-            <div className="logo-navbar"> ADP<span>.</span>
-            </div>
-            <div className="navbar-link">
-                <span className="item-navbar">Movies</span>
-                <span className="item-navbar">My List</span>
-            </div>
-            <i class="nav-search2"></i>
-            <div class="search-form" id="searchform">
-				<form action="" method="post">
-					<input type="text" name="search" placeholder="Search here" id="keyword"/>
-				</form>
-			</div>
-            </div>
-            <div className="right">
-            <span>Login</span>
-            <div className="profile">
-                <div className="options">
-                <span>Settings</span>
-                <span>Logout</span>
+            <div className="container">
+                <div className="left">
+                    <div className="logo-navbar">ADP<span>.</span>
+                    </div>
+                    <div className="navbar-link">
+                        <span className="item-navbar">Movies</span>
+                        <span className="item-navbar">My List</span>
+                    </div>
+                    <i class="nav-search2"></i>
+                    <div class="search-form" id="searchform">
+                        <form action="" method="post">
+                            <input type="text" name="search" placeholder="Search here" id="keyword"/>
+                        </form>
+                    </div>
+                </div>
+                <div className="right">
+                    Login
+                    <div className="profile">
+                        <div className="options">
+                        <span>Settings</span>
+                        <span>Logout</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
         </div>
     )
 }
