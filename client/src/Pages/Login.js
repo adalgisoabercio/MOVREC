1320f6e31955a31fd14fcab8ee131c20d50e6091
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-12-05 22:19:04 +0700

    Add files via upload

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,30 @@
+import "./ast/Login.css";
+import Google from "./ast/google.png";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <div className="logo">
+            ADP <span>.</span>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or phone number" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+          <span>
+            New? <b>Sign up now.</b>
+          </span>
+          <button className="loginButton-google">
+            <img src={Google} width="23" height="23" alt="cam"/>Sign In Using Google</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
