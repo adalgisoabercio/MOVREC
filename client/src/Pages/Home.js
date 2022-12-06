ae52b2e26519c02a2e50c3f11a7747ea08b00313
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-12-06 07:13:24 +0700

    Update Home.js

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import Recommendation from '../Components/Recommendation';
 import './Style/Home.css';
 
 const Home = () => {
     return(
         <div class="Home-Container">
             <Navbar />
+            <Recommendation />
         </div>
     );
 
