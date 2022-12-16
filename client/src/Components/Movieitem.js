b658cab19ffb4ecbb9066b9de04afd5adac1b0f0
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-12-16 15:52:31 +0700

    Update Movieitem.js

--- client/src/Components/Movieitem.js
+++ client/src/Components/Movieitem.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-import "./Movieitem.css";
+import "./style/Movieitem.css";
 import "slick-carousel/slick/slick-theme.css";
 import { DataMovie } from "./data";
 
