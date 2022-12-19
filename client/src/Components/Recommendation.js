1669adfd97529a7af8065ebf01b42ed0fd5c55f8
vanes16 <110099784+vanes16@users.noreply.github.com>
2022-12-19 14:47:06 +0700

    Update Recommendation.js

--- client/src/Components/Recommendation.js
+++ client/src/Components/Recommendation.js
@@ -1,23 +1,87 @@
+import React from "react";
+import HeroSlider, { Slide, Nav } from "hero-slider";
+import { PlayArrow } from "@material-ui/icons";
+// Images
 import "./style/Recommendation.css";
-import IMG from "../Assets/movie_poster.jpg";
-export default function Recommendation() {
-  return (
-    <div className="featured">
-      <img className="image"
-        src={IMG}
-      />
-      <div className ="data-content">
-        <div className="left-content">
-          <div>NAMA FILM</div>
 
+const Recommendation = () => {
+  return (
+    <HeroSlider
+      height={"100vh"}
+      autoplay
+      controller={{
+        initialSlide: 1,
+        slidingDuration: 500,
+        slidingDelay: 100,
+        onSliding: (nextSlide) =>
+          console.debug("onSliding(nextSlide): ", nextSlide),
+        onBeforeSliding: (previousSlide, nextSlide) =>
+          console.debug(
+            "onBeforeSliding(previousSlide, nextSlide): ",
+            previousSlide,
+            nextSlide
+          ),
+        onAfterSliding: (nextSlide) =>
+          console.debug("onAfterSliding(nextSlide): ", nextSlide)
+      }}
+    >
+      <Slide
+        shouldRenderMask
+        navDescription="ADP 2022"
+        background={{
+          backgroundImageSrc: "https://i.imgur.com/g5JNdYL.jpg"
+        }}
+      >
+        <div className="heroContent">
+          <div className="leftContent">
+            <h1 className="title">Movie Name</h1>
+            <div className="buttons">
+              <button className="play">
+                <PlayArrow /> <span>Play Now</span>
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="right-content">
-          <img
-                src={IMG} 
-                alt=""
-              />
+      </Slide>
+      <Slide
+        shouldRenderMask
+        navDescription="ADP 2022"
+        background={{
+          backgroundImageSrc: "https://i.imgur.com/XpreppQ.jpeg"
+        }}
+      >
+        <div className="heroContent">
+          <div className="leftContent">
+            <h1 className="title">Movie Name</h1>
+            <div className="buttons">
+              <button className="play">
+                <PlayArrow /> <span>Play Now</span>
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </Slide>
+      <Slide
+        shouldRenderMask
+        navDescription="ADP 2022"
+        background={{
+          backgroundImageSrc: "https://i.imgur.com/VJhOuhK.jpeg"
+        }}
+      >
+        <div className="heroContent">
+          <div className="leftContent">
+            <h1 className="title">Movie Name</h1>
+            <div className="buttons">
+              <button className="play">
+                <PlayArrow /> <span>Play Now</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </Slide>
+      <Nav />
+    </HeroSlider>
   );
-}
+};
+
+export default Recommendation;
